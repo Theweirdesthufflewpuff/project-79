f442e3e8855fe0a5bf3a165978096900eb3082f2
Theweirdesthufflewpuff <75901727+Theweirdesthufflewpuff@users.noreply.github.com>
2021-03-25 20:17:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,25 @@
-var last_position_of_x,last_position_of_y;
-canvas=document.getElementById('myCanvas');
-ctx=canvas.getContext("2d");
-color="black";
-width_of_line=1;
-var width=screen.width;
-new_width=screen.width-70;
-new_height=screen.height-300;
-if(width<9920)
-{
-document.getElementById("myCanvas").width=new_width;
-document.getElementById("myCanvas").height=new_height;
-document.body.style.overflow="hidden";
-}
+name_of_student_array=[];
+function submit() {
 
-canvas.addEventListener("touchstart",my_touchstart);
-function my_touchstart(e){
-color=document.getElementById("color").value;
-width_of_line=document.getElementById("width_of_line").value;
-console.log("my_touchstart");
-last_position_of_x=e.touches[0].clientX-canvas.offsetLeft;
-last_position_of_y=e.touches[0].clientY-canvas.offsetTop;         
+ var name_1=document.getElementById("pizza_1").value;
+ var name_2=document.getElementById("pizza_2").value;
+ var name_3=document.getElementById("pizza_3").value;
+ var name_4=document.getElementById("pizza_4").value;
+ var name_5=document.getElementById("pizza_5").value;
+ name_of_student_array.push(name_1);
+ name_of_student_array.push(name_2);
+ name_of_student_array.push(name_3);
+ name_of_student_array.push(name_4);
+ name_of_student_array.push(name_5);
+ console.log(name_of_student_array);
+ document.getElementById
+ ("display_name").innerHTML=name_of_student_array;
+ document.getElementById("submit_button").style.display="none";
+ document.getElementById("sort_button").style.display="inline-block";
 }
-canvas.addEventListener("touchmove",my_touchmove);
-function my_touchmove(e){
-current_position_of_touch_x=e.touches[0].clientX-canvas.offsetLeft;
-current_position_of_touch_y=e.touches[0].clientY-canvas.offsetTop;
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.linewidth=width_of_line
-console.log("Last position of x and y coordinates = "); 
-console.log("x = " + last_position_of_x + "y = " + last_position_of_y); 
-ctx.moveTo(last_position_of_x, last_position_of_y); 
-console.log("Current position of x and y coordinates = "); 
-console.log("x = " + current_position_of_touch_x + "y = " + current_position_of_touch_y); 
-ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y); 
-ctx.stroke();
-last_position_of_x=current_position_of_touch_x;
-last_position_of_y=current_position_of_touch_y;
-}
-function cleararea(){
-ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+function sorting()
+{
+name_of_student_array.sort();
+console.log(name_of_student_array);
+document.getElementById("display_name").innerHTML=name_of_student_array;
 }
